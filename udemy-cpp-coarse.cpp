4e9f1a443a56203acb1681dc7bda42849d2f5676
rmacomber <ryanmacomber@outlook.com>
2020-08-16 08:59:25 -0400

    udemy code

---
+++ udemy-cpp-coarse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string text1 = "hello ";
+	string text2 = "fred";
+
+	string3 = text1 + text2;
+
+	cout << text1 << text2 << endl;
+
+	cout << text3 << endl;
+
+	return 0;
+}
