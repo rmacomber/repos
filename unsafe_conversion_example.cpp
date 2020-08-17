c05a2015dc26fe3782f22d84ae5d74074594fa72
rmacomber <ryanmacomber@outlook.com>
2020-08-17 13:14:20 -0400

    corrected unsafe_conversion_example

--- unsafe_conversion_example.cpp
+++ unsafe_conversion_example.cpp
@@ -1,8 +1,14 @@
-#include "std_lib_facilities.h";
+#include "std_lib_facilities.h"
+//#include <iostream>
+//using namespace std;
 
 int main() 
 {
 	double d = 0;
-	while(cin>>4)
-	return 0;
+	while (cin>>d) {
+		int i = d;
+		char c = i;
+		int i2 = c;
+		std::cout<<"d=="<<d<<" i=="<<i<<" i2=="<<i2<<" char("<<c<<")\n";
+	}
 }
