aa130a7c33555cbbfaccd69e6787e032d8fc2239
rmacomber <ryanmacomber@outlook.com>
2020-09-08 09:26:16 -0400

    dfhrty

---
+++ MyWorkSpace/fuckaround/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+	cout << "My template" << endl;
+    
+    //subnet calculator
+    
+    double ip_adress{}; 
+    cout << "Please enter the ip address: ";
+    cin
+	return 0;
+}
